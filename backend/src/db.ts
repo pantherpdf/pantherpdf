67c0ceaa44afb3067c27b66818cadb4db42567b7
ibanic <ibanic@users.noreply.github.com>
2021-07-16 12:54:38 +0200

    db timeout and unifiedTopology

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -7,7 +7,11 @@ export default async function connectToDatabase() {
 		return cachedDb;
 	}
 	const MONGODB_URI = `mongodb+srv://${encodeURIComponent(process.env.MONGODB_USER||'')}:${encodeURIComponent(process.env.MONGODB_PASSWORD||'')}@${process.env.MONGODB_URL||''}/${encodeURIComponent(process.env.MONGODB_DB||'')}?retryWrites=true&w=majority`;
-	const client = await MongoClient.connect(MONGODB_URI);
+	const client = await MongoClient.connect(MONGODB_URI, {
+		useUnifiedTopology: true,
+		connectTimeoutMS: 3000,
+		serverSelectionTimeoutMS: 3000,
+	});
 	const db = client.db(process.env.MONGODB_DB);
 	cachedDb = db;
 	return db;
