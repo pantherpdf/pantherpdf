2bb0373bea3f733fb10ae27bf6808da11514a1b4
ibanic <ibanic@users.noreply.github.com>
2021-07-16 20:56:46 +0200

    router

--- frontend/src/AppContainer.tsx
+++ frontend/src/AppContainer.tsx
@@ -3,11 +3,13 @@ import './global.scss'
 import './App.css'
 import Login from './pages/Login'
 import { AppContext, getApp, IAppContextData, AppContextDefaultData } from './context'
-import { BrowserRouter, Route, Redirect, RouteProps } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, RouteProps, Switch } from 'react-router-dom'
 import App from './App'
 import Dashboard from './pages/Dashboard'
 import Keys from './pages/Keys'
 import Settings from './pages/Settings'
+import Report from './pages/Report'
+import NotFound from './pages/NotFound'
 
 
 function PrivateRoute (props1: RouteProps) {
@@ -28,7 +30,7 @@ function PrivateRoute (props1: RouteProps) {
 function AppContainer() {
 	const [data, setData] = useState<IAppContextData>(AppContextDefaultData())
 	const app = getApp(data, setData);
-	const [loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState<boolean>(true)
 
 	useEffect(() => {
 		const sid = window.localStorage.getItem('sid')
@@ -50,11 +52,15 @@ function AppContainer() {
 
 	return <AppContext.Provider value={app}>
 		<BrowserRouter>
-			<Route exact path='/'><Redirect to='/dashboard' /></Route>
-			<Route exact path='/login' component={Login} />
-			<PrivateRoute exact path='/dashboard' component={Dashboard} />
-			<PrivateRoute exact path='/keys' component={Keys} />
-			<PrivateRoute exact path='/settings' component={Settings} />
+			<Switch>
+				<Route exact path='/'><Redirect to='/dashboard' /></Route>
+				<Route exact path='/login' component={Login} />
+				<PrivateRoute exact path='/dashboard' component={Dashboard} />
+				<PrivateRoute exact path='/keys' component={Keys} />
+				<PrivateRoute exact path='/settings' component={Settings} />
+				<PrivateRoute exact path='/reports/:id' component={Report} />
+				<Route component={NotFound} />
+			</Switch>
 		</BrowserRouter>
 	</AppContext.Provider>
 }
