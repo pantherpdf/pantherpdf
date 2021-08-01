95760046ab9214a58b3dbfbf8ab74b766383c1af
ibanic <ibanic@users.noreply.github.com>
2021-08-01 11:28:34 +0200

    worker updated api

--- worker/server.py
+++ worker/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+import flask
 import shutil
 import threading
 import time
@@ -8,6 +8,15 @@ import chromeHelper
 import signal
 import random
 import string
+import dotenv   # pip install python-dotenv
+import hmac
+import hashlib
+
+
+# env file
+dotenv.load_dotenv()
+SECRET = os.getenv('SECRET')
+assert isinstance(SECRET, str) and len(SECRET) > 0
 
 
 # find chrome path
@@ -33,7 +42,7 @@ mutexTmpFiles = threading.Lock()
 
 
 # web server
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 # where to save PDFs
@@ -54,12 +63,11 @@ def convertThrd(data, id2):
 		data['status'] = 'working'
 
 	targetPath = data['path']
-	url = data['url']
-	print('Printig ', url, ' to ', targetPath)
+	print('Printig ', data['properties']['url'], ' to ', targetPath)
 
 	try:
 		# convert
-		chromeHelper.convertHtmlToPdf(chromePth, data, targetPath)
+		chromeHelper.convertHtmlToPdf(chromePth, data['properties'], targetPath)
 
 		# add watermark
 		x = { 'text': 'KELGRAND Reports' }
@@ -138,28 +146,60 @@ def startCleaningJob():
 
 
 
-@app.route('/')
-def hello_world():
-	return 'Hello, World!', 200
+def sign(msg, secret_key):
+	if isinstance(msg, str):
+		msg = msg.encode('utf-8')
+	assert isinstance(msg, bytes)
+	if isinstance(secret_key, str):
+		secret_key = secret_key.encode('utf-8')
+	assert isinstance(secret_key, bytes)
+	return hmac.new(secret_key, msg=msg, digestmod=hashlib.sha256).hexdigest()
+
+
+
+def verify(msg, secret_key, signature_verify):
+	signature_calc = sign(msg, secret_key)
+	return hmac.compare_digest(signature_calc, signature_verify)
+
 
 
 @app.route('/apiv1/convert', methods=['POST'])
 def apiConvert():
 	global pthDir, mutexTmpFiles, tmpDocuments
 
+	# get request signature
+	signature_verify = flask.request.headers.get('x-signature') or ''
+
+	# verify signature
+	rqBody = flask.request.get_data()
+	if not verify(rqBody, SECRET, signature_verify):
+		return {'msg':'Bad or missing header x-signature'}, 403
+
+	# verify request
+	rqJs = flask.request.json
+	if not isinstance(rqJs, dict):
+		return {'msg':'Bad request body'}, 400
+	if 'url' not in rqJs or not isinstance(rqJs['url'], str):
+		return {'msg':'Missing or bad url in request'}, 400
+	url = rqJs['url']
+	if not url.startswith('https://') and not url.startswith('http://'):
+		return {'msg':'Bad url protocol'}, 400
+	# todo check valid url
+	# do not allow localhost or 127.0.0.1 or ::1 or ::1/128 or similar
+	if 'fileName' in rqJs:
+		# todo validate fileName
+		pass
+
+	print('Got request', rqJs)
+
 	js = {
-		'url': 'http://www.google.com/',
+		'properties': rqJs,
 		'status': 'waiting',
 		'errorMsg': None,
 		'time': time.time(),
 		'keepUntil': time.time() + 60*60*15,  # keep 15 min
 		'path': None,
 	}
-	print('Got requeest', js)
-
-	# check valid url
-	# do not allow localhost or 127.0.0.1 or ::1 or ::1/128 or similar
-	# todo
 
 	# random id
 	with mutexTmpFiles:
@@ -175,6 +215,51 @@ def apiConvert():
 	t.start()
 
 	return {'id':id2}, 200
+
+
+
+@app.route('/apiv1/status/<id2>', methods=['GET'])
+def apiStatus(id2):
+	global mutexTmpFiles, tmpDocuments
+	with mutexTmpFiles:
+		try:
+			obj = tmpDocuments[id2]
+		except KeyError:
+			return {'msg':'id doesnt exist'}, 404
+		res = {}
+		res['status'] = obj['status']
+		if obj['errorMsg']:
+			res['errorMsg'] = obj['errorMsg']
+		return res, 200
+
+
+
+@app.route('/apiv1/download/<id2>', methods=['GET'])
+def apiDownload(id2):
+	global mutexTmpFiles, tmpDocuments, app
+	with mutexTmpFiles:
+		try:
+			obj = tmpDocuments[id2]
+		except KeyError:
+			return {'msg':'document doesnt exist'}, 404
+		if obj['status'] != 'finished':
+			return {'msg':'document is not finished yet.'}, 400
+		if obj['errorMsg']:
+			return {'msg':'Document is not available due to error: '+obj['errorMsg']}, 400
+		fileName = obj['properties']['fileName'] if 'fileName' in obj['properties'] else f'{id2}.pdf'
+		return flask.send_file(obj['path'], mimetype='application/pdf', as_attachment=False, download_name=fileName), 200
+
+
+
+@app.errorhandler(404)
+def apiErrorNotFound(e):
+	return {'msg': str(e)}, 404
+
+
+
+@app.errorhandler(500)
+def apiErrorServer(e):
+	return {'msg': str(e)}, 500
 
 
 
