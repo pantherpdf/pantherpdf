cb56eafd10c6e34c2019a4b8e285ef09217d13f7
ibanic <ibanic@users.noreply.github.com>
2021-08-03 22:53:42 +0200

    shared enable react-scripts start

---
+++ shared/src/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './editor/Editor';
+import { sampleReport } from './editor/sampleReport';
+import { ApiEndpoints, TReport } from './types';
+
+function App() {
+	const [report, setReport] = useState<TReport>(sampleReport)
+	const orgiData = {
+		abc: 123,
+	}
+
+	const api: ApiEndpoints = {
+		reportGet: (id) => { throw new Error('') },
+		files: async () =>  { throw new Error('') },
+		filesDelete: async (name) => { },
+		filesUpload: async (file, data, cbProgress) => { },
+		filesDownloadUrl: (name) => { throw new Error('') },
+	}
+
+	return <Editor
+		report={report}
+		setReport={async (r) => setReport(r)}
+		deleteReport={() => {}}
+		allReports={[]}
+		getOriginalSourceData={() => orgiData}
+		api={api}
+	/>
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
