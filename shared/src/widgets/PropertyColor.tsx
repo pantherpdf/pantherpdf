d4edab66d147ae2c570b762d920cf01efd244165
ibanic <ibanic@users.noreply.github.com>
2021-08-05 20:24:21 +0200

    PropertyColor call onChange() only when mouse is released

--- shared/src/widgets/PropertyColor.tsx
+++ shared/src/widgets/PropertyColor.tsx
@@ -3,7 +3,7 @@
  */
 
 
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { HexColorPicker } from 'react-colorful'
 import style from './PropertyColor.module.css'
 
@@ -14,14 +14,36 @@ interface Props {
 
 export default function PropertyColor(props: Props) {
 	const [show, setShow] = useState<boolean>(false)
+	const [value, setValue] = useState<string>(props.value)
+
+	useEffect(() => {
+		setValue(props.value)
+	}, [props.value])
+
+	// must use global listener
+	// div.onMouseUp() is not called when user releases mouse outside of color selector
+	useEffect(() => {
+		window.document.documentElement.addEventListener('mouseup', mouseup);
+		return () => {
+			window.document.documentElement.removeEventListener('mouseup', mouseup);
+		}
+	})
+	const mouseup = () => {
+		if (value !== props.value) {
+			props.onChange(value)
+		}
+	}
 
 	return <div>
 		<div className={ style.swatch } onClick={() => setShow(!show)}>
-			<div className={ style.color } style={{backgroundColor:props.value}} />
+			<div className={ style.color } style={{backgroundColor:value}} />
 		</div>
-		<div className={ style.popover }>
+		{show && <div className={ style.popover }>
 			<div className={ style.cover } onClick={() => setShow(false)} />
-			<HexColorPicker color={props.value} onChange={props.onChange} />
-		</div>
+			<HexColorPicker
+				color={value}
+				onChange={setValue}
+			/>
+		</div>}
 	</div>
 }
