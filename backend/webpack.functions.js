a7ea2002318644c1563112165755576672e09de5
Ignac Banič <ignacb@gmail.com>
2021-08-19 22:48:49 +0200

    Update webpack.functions.js

--- backend/webpack.functions.js
+++ backend/webpack.functions.js
@@ -2,12 +2,16 @@ const Dotenv = require('dotenv-webpack')
 const webpack = require('webpack')
 const fs = require('fs')
 
+const dotenvConf = {
+  safe: false,
+}
+if (fs.existsSync('../.env')) {
+  dotenvConf.path = '../.env'
+}
+
 module.exports = {
   plugins: [
-    new Dotenv({
-      path: (fs.existsSync('../.env') ? '../.env' : undefined),
-      safe: false,
-    }),
+    new Dotenv(dotenvConf),
 
     // prevent from importing canvas - its not installed and would not work with lambda
     new webpack.IgnorePlugin(/canvas/, /jsdom$/),
