19280ca2fe245b4222c09cc6c09cf76ebc7daf49
Ignac Banič <ignacb@gmail.com>
2021-09-05 20:57:55 +0200

    Remove frontend and backend

--- shared/src/index-real.ts
+++
@@ -1,9 +0,0 @@
-import FileDialog from './FileDialog'
-import Editor from './editor/Editor'
-import compile from './editor/compile'
-import makeHtml from './editor/makeHtml'
-import { PropertyFontGenCss } from './widgets/PropertyFont'
-import { transformData } from './editor/DataTransform'
-
-export * from './types'
-export { Editor, FileDialog, compile, makeHtml, PropertyFontGenCss, transformData }
