b8b68fc812b6871b98ed34e0d6290cf8fc854ae6
Ignac Banič <ignacb@gmail.com>
2021-12-20 16:33:51 +0100

    export DataObj, DataTypes

--- src/index-package.ts
+++ src/index-package.ts
@@ -5,7 +5,19 @@ import makeHtml from './editor/makeHtml'
 import { PropertyFontGenCss } from './widgets/PropertyFont'
 import { transformData } from './editor/DataTransform'
 import getOriginalSourceData from './editor/getOriginalSourceData'
+import type { DataTypes, DataObj } from './editor/getOriginalSourceData'
 
 export * from './types'
 export * from './editor/generateTarget'
-export { Editor, FileDialog, compile, makeHtml, PropertyFontGenCss, transformData, getOriginalSourceData, uploadFile }
+export {
+	Editor,
+	FileDialog,
+	compile,
+	makeHtml,
+	PropertyFontGenCss,
+	transformData,
+	getOriginalSourceData,
+	uploadFile,
+	DataTypes,
+	DataObj,
+}
